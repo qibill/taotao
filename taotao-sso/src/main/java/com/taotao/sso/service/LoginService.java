871562ead097b11529f98a98be9b9d83db7a4b61
qibill <870937917@qq.com>
2017-10-30 17:07:14 +0800

    1.新建项目

---
+++ taotao-sso/src/main/java/com/taotao/sso/service/LoginService.java
@@ -0,0 +1,29 @@
+package com.taotao.sso.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.taotao.commom.pojo.TaotaoResult;
+
+public interface LoginService {
+
+	/**
+	 * <P> @Title: login
+	 * <P> @Description:登入
+	 * @param username
+	 * @param password
+	 * @param request
+	 * @param response
+	 * @return TaotaoResult
+	 */ 
+	TaotaoResult login(String username, String password, HttpServletRequest request,
+			HttpServletResponse response);
+	
+	/**
+	 * <P> @Title: getUserByToken
+	 * <P> @Description:通过token查询用户信息
+	 * @param token
+	 * @return TaotaoResult
+	 */ 
+	TaotaoResult getUserByToken(String token);
+}
---
+++ taotao-sso/src/main/java/com/taotao/sso/service/RegisterService.java
@@ -0,0 +1,24 @@
+package com.taotao.sso.service;
+
+import com.taotao.commom.pojo.TaotaoResult;
+import com.taotao.pojo.TbUser;
+
+public interface RegisterService {
+	
+	/**
+	 * <P> @Title: checkData
+	 * <P> @Description: 检查数据
+	 * @param param
+	 * @param type
+	 * @return TaotaoResult
+	 */ 
+	TaotaoResult checkData(String param, int type);
+	
+	/**
+	 * <P> @Title: register
+	 * <P> @Description: 用户登入
+	 * @param user
+	 * @return TaotaoResult
+	 */ 
+	TaotaoResult register(TbUser user);
+}
