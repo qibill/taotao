6fe09626c3a2468e845bbd37d4265657d77fb4e2
qibill <870937917@qq.com>
2017-10-30 17:05:55 +0800

    1.新建项目

---
+++ taotao-search/src/main/java/com/taotao/search/controller/ItemController.java
@@ -0,0 +1,30 @@
+package com.taotao.search.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.commom.pojo.TaotaoResult;
+import com.taotao.commom.untils.ExceptionUtil;
+import com.taotao.search.service.ItemService;
+
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/importall")
+	@ResponseBody
+	public TaotaoResult importAll() {
+		try {
+			TaotaoResult result = itemService.importItems();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+	}
+
+}
---
+++ taotao-search/src/main/java/com/taotao/search/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.taotao.search.service;
+
+import com.taotao.commom.pojo.TaotaoResult;
+
+public interface ItemService {
+
+	/**
+	 * <P> @Title: importItems
+	 * <P> @Description: 导入数据
+	 * @return TaotaoResult
+	 */ 
+	TaotaoResult importItems() throws Exception;
+}
